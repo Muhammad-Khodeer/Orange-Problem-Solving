eb33b7f76c4fe3766399514ffe4bcc10f20f06c7
Muhammad_Khodeer <muhammad.khodeer@gmail.com>
2017-07-17 21:01:35 +0200

    Add files via upload

---
+++ ThunderBolt/ThunderBolt/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThunderBolt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testNum = int.Parse(Console.ReadLine());
+            int[] outputs = new int[testNum];
+
+            for (int i = 0; i < testNum; i++)
+            {
+                string taken = Console.ReadLine();
+                int start = int.Parse(taken.Split(' ')[0]);
+                int end = int.Parse(taken.Split(' ')[1]);
+
+                for (int j = start; j <= end; j++)
+                {
+                    if ((isPrime(j)) && isPrime(sumDigits(j)))
+                        outputs[i] += 1;
+                }
+            }
+
+            for (int i = 0; i < testNum; i++)
+                Console.WriteLine(outputs[i]);
+
+            Console.ReadKey();
+
+
+        }
+        // is prime method
+        static public Boolean isPrime(int num)
+        {
+            if (num < 2) return false;
+            if (num % 2 == 0) return (num == 2);
+            int root = (int)Math.Sqrt((double)num);
+
+            for (int a = 3; a <= root; a += 2)
+                if (num % a == 0)
+                    return false;
+
+            return true;
+        }
+        // sum of digit methos
+        static public int sumDigits(int num)
+        {
+            string digits = num.ToString();
+            int sum = 0;
+            for (int j = 0; j < digits.Length; j++)
+            {
+                sum += (int)(digits[j] - '0');
+            }
+            return sum;
+        }
+    }
+}
---
+++ work_hours/work_hours/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace work_hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testNum = int.Parse(Console.ReadLine());
+            int[] outputs = new int[testNum];
+           
+
+
+            for (int i = 0; i < testNum; i++) // every test case
+            {
+                /***/
+                int workers = int.Parse(Console.ReadLine());
+
+                int[] starts = new int[workers];
+                int[] ends = new int[workers];
+
+                for (int k = 0; k < workers; k++)
+                {
+                    string taken = Console.ReadLine();
+                    starts[k] = int.Parse(taken.Split(' ')[0]); // an array of start time for each worker
+                    ends[k] = int.Parse(taken.Split(' ')[1]); // an array of end time for each worker 
+                }
+                /***/
+
+                // sort the first and second array 
+
+                Array.Sort<int>(starts,
+                new Comparison<int>(
+                        (i1, i2) => i1.CompareTo(i2)
+                ));
+
+                Array.Sort<int>(ends,
+                new Comparison<int>(
+                        (i1, i2) => i1.CompareTo(i2)
+                ));
+
+                // 
+
+                outputs[i] = 0;
+                outputs[i] += ends[0] - starts[0];
+
+                // loop to add each differance between the two arrays
+                // but u have to check may be the start of the scond worker is less than the end of the first worker .
+
+                for (int m = 1; m < workers; m++)
+                {
+                    if (starts[m] < ends[m - 1])    
+                        outputs[i] += ends[m] - ends[m - 1];
+
+                    if (starts[m] >= ends[m - 1])          
+                        outputs[i] += ends[m] - starts[m];
+                }
+
+                /***/
+            }
+
+
+            for (int i = 0; i < testNum; i++)
+                Console.WriteLine(outputs[i]);
+
+        }
+    }
+}
